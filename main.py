97e3909c03574ef03f13adec73c59a6dfc82bb5e
hsie7k2hwos-hue <hsie7k2hwos@gmail.com>
2026-09-09 09:26:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -654,7 +654,7 @@ async def get_card_handler(message: Message):
         
         if status == "all_collected":
             await message.reply(
-                f"<blockquote><b>🎉 {nickname}, вы собрали все доступные карточки на данный момент!</b> Пожалуйста, дождитесь добавления новых</blockquote>"
+                f"<blockquote><b><tg-emoji emoji-id='{5436040291507247633}'>🎉</tg-emoji> {nickname}, вы собрали все доступные карточки на данный момент!</b> Пожалуйста, дождитесь добавления новых</blockquote>"
             )
             return
         elif status == "error" or card_data is None:
@@ -666,9 +666,9 @@ async def get_card_handler(message: Message):
         # Отправляем карточку
         rarity_title = RARITIES[card_data["rarity"]]["name"]
         caption = (
-            f"<blockquote><b>🎉 {nickname}</b>, вам выпала новая карточка: <b>{card_data['name']}</b>\n\n"
+            f"<blockquote><b><tg-emoji emoji-id='{5436040291507247633}'>🎉</tg-emoji> {nickname}</b>, вам выпала новая карточка: <b>{card_data['name']}</b>\n\n"
             f"🎲 Редкость: <b>{rarity_title}</b>\n"
-            f"🩷 Милота: <b>+{card_data['coins_earned']} (всего: {card_data['balance']})</b></blockquote>"
+            f"<tg-emoji emoji-id='{5434031913260035048}'>🩷</tg-emoji> Милота: <b>+{card_data['coins_earned']} (всего: {card_data['balance']})</b></blockquote>"
         )
         
         await message.reply_photo(
@@ -682,7 +682,7 @@ async def get_card_handler(message: Message):
         if bonus > 0 and streak > 0:
             await message.reply(
                 f"<blockquote>🔥 <b>{nickname}</b>, ваш стрик <b>{streak} день</b>\n"
-                f"🩷 Бонус за стрик: <b>+{bonus} милоты (всего: {new_balance})</b></blockquote>"
+                f"<tg-emoji emoji-id='{5434031913260035048}'>🩷</tg-emoji> Бонус за стрик: <b>+{bonus} милоты (всего: {new_balance})</b></blockquote>"
             )
             
     except Exception as e:
